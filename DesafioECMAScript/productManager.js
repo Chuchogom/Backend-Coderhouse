d283b0fc4ce0ef615a46de4d6079dd3d744ae966
Jesús Gómez <90487411+Chuchogom@users.noreply.github.com>
2023-06-06 18:34:01 -0600

    Modificaciones primer desafio

--- DesafioECMAScript/productManager.js
+++ DesafioECMAScript/productManager.js
@@ -4,31 +4,56 @@ class ProductManager {
         this.products = []
     }
 
+    //Get all the products
     getProducts = () => {
         return this.products
     }
 
+    //Search a product by id
     productId = () => {
         const count = this.products.length 
         const nextId = (count > 0) ? this.products[count-1].id + 1 : 1
         return nextId
     }
 
-    addProduct = (title, description, price,thumbnail,stock) => {
-        const id = this.productId()
-        const code = `PRD-${this.productId()}`
-        const product = {
-            id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code, 
-            stock: stock ?? 50
-        }
-        this.products.push(product)
+    //Add a new product
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+      const id = this.productId()
+      const code = `PRD-${this.productId()}`
+      const product = {
+        id,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock: stock ?? 50,
+      }
+
+      // Check if all parameters are defined
+      if (
+        title === undefined ||
+        description === undefined ||
+        price === undefined ||
+        thumbnail === undefined ||
+        stock === undefined ||
+        code === undefined
+      ) {
+        throw new Error('All arguments are required')
+      }
+
+      // Check if the code already exists
+      const productWithSameCode = this.products.find(
+        (product) => product.code === code
+      )
+      if (productWithSameCode) {
+        throw new Error('The code already exists')
+      }
+
+      this.products.push(product)
     }
 
+    //Get all the product info with an id
     getProductById = (productId) => {
         const product = this.products.find(product => product.id == productId)
 
