5cf84d357039670f6882dbf3e5961b0edfb1e138
jesusgomrec <jesus.gomrec@gmail.com>
2023-06-26 11:51:45 -0600

    Modificaciones Desafio Express

--- DesafioExpress/app.js
+++ DesafioExpress/app.js
@@ -14,42 +14,67 @@ app.get('/', (req,res) => {
 app.get('/products', async (req, res) => {
     try {
         const limit = req.query.limit
-        const products = await productManager.getProducts(limit)
-        res.json(products)
+        if (limit) {
+            const products = await productManager.getProducts(limit)
+            res.json(products)
+        } else {
+            const products = await productManager.getProducts()
+            res.json(products)
+        }
     } catch (error) {
+        console.error(error)
         res.status(500).send(error.message)
     }
 })
 
 //Add a new product
 app.post('/products', async (req, res) => {
-    const { title, description, price, thumbnail, stock } = req.body
-    await productManager.addProduct(
-        title, description, price, thumbnail, stock
-    )
-    res.json({status: 'success', message: 'Product added succesfully'})
+    try {
+        const { title, description, price, thumbnail, stock } = req.body
+        await productManager.addProduct(
+            title, description, price, thumbnail, stock
+        )
+    } catch (error) {
+        console.error(error);
+        res.json({ status: 'success', message: 'Product added succesfully' })
+    }
 })
 
 //Get a product by id
 app.get('products/:id', async (req,res)=>{
     const productId = req.params.id
-    const product =  await productManager.getProductById(productId)
-    res.send(product)
+    try {
+        const product =  await productManager.getProductById(productId)
+        res.send(product)   
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error.message)
+    }
 })
 
 //Update a product by id
 app.put('/products/:id', async (req, res) => {
     const productId = req.params.id
     const updatedProduct = req.body
-    await productManager.updateProduct(productId, updatedProduct)
-    res.json({status: 'success', message: 'Product updated successfully'})
+    try {
+        await productManager.updateProduct(productId, updatedProduct)
+        res.json({status: 'success', message: 'Product updated successfully'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error.message)
+    }
 })
 
 //Delete a product by id
 app.delete('/products/:id', async (req, res) => {
     const productId = req.params.id
-    await productManager.deleteProduct(productId)
-    res.json({status: 'sucsess', message: 'Product deleted'})
+    try {
+        await productManager.deleteProduct(productId)
+        res.json({status: 'sucsess', message: 'Product deleted'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error.message)
+    }
 })
 
 
